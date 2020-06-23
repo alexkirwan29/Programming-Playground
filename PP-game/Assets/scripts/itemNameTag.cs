063a65786160ae9cb5b2df0232a9014fdce207e9
alexkirwan29 <alexkirwan29@gmail.com>
2020-06-23 22:15:24 +0800

    cool things are happening...
    alked about appception, propgun app, apps.. do you want to build and app.

--- PP-game/Assets/scripts/itemNameTag.cs
+++ PP-game/Assets/scripts/itemNameTag.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class itemNameTag : MonoBehaviour
 {
 
-    public string itemName = "";
-    public string itemID = "";
-
-  
-
+    [SerializeField] TextMeshProUGUI item_id;
+    [SerializeField] TextMeshProUGUI item_name;
+    [SerializeField] TextMeshProUGUI item_value;
 
     // Start is called before the first frame update
     void Start()
     {
 
-     
-
-        
+        item_id.text    = "289";     
+        item_name.text  = "blue_block";     
+        item_value.text = "$0";     
 
     }
 
---
+++ PP-game/Assets/scripts/spawnGun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spawnGun : MonoBehaviour
+{
+
+       
+    //ui interactivity
+    public Text spawnerFeedback;
+    public InputField itemIdInput;
+
+    public GameObject item1; //id 1
+    public GameObject item2; //id 2
+
+    public bool isValidItem;
+
+    public int selectedid = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+             
+
+
+    }
+}
