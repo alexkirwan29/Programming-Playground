580048b270e444e10dec0ec60ff12d22fa5bf138
Tom Parker <tom@pop-os.localdomain>
2020-07-04 12:38:42 +0800

    Changes from laptop to desktop

--- server-side/pp-api/api_v1/inventories.py
+++ server-side/pp-api/api_v1/inventories.py
@@ -1,6 +1,17 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
+from api_v1 import connection
+
 class Inventory(Resource):
 	def get(self, inv_id):
-		return inv_id
+		with connection.cursor() as cursor:
+			query = "SELECT * FROM inventories WHERE inv_id = %s"
+			# query = "SELECT item_id, quantity FROM inventory_map WHERE inv_id = %s"
+			cursor.execute(query, inv_id)
+
+			result = cursor.fetchone()
+
+			result = (result, ("items":2))
+
+			return list(result)
