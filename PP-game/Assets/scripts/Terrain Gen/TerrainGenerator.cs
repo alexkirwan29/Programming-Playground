6448ab62b1b90ff880c1bcffa4046d6bbac387a7
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-07-05 22:17:19 +0800

    Basic terrain generation complete

--- PP-game/Assets/scripts/Terrain Gen/TerrainGenerator.cs
+++ PP-game/Assets/scripts/Terrain Gen/TerrainGenerator.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System;
 
 [CustomEditor(typeof(TerrainGenerator))]
 public class TerrainGeneratorCustomInspector : Editor {
-    TerrainGenerator tg;
-
     public override void OnInspectorGUI() {
         if (GUILayout.Button("Generate New Terrain")) {
-            if (tg == null)
-                tg = new TerrainGenerator();
-            tg.GenerateNewTerrain();
+            TerrainGenerator tg = new TerrainGenerator();
+            tg.GenerateNewTerrain(
+                150, 0.15f,
+                50, 0.07f,
+                25, 0.03f
+            );
         }
     }
 }
 
 public class TerrainGenerator : MonoBehaviour {
-    public void GenerateNewTerrain() {
+    public void GenerateNewTerrain(float scale_1, float height_1, float scale_2, float height_2, float scale_3, float height_3) {
         //We want to actually generate the terrain now
         Terrain terrain = Selection.activeGameObject.GetComponent<Terrain>();
         TerrainData terrainData = terrain.terrainData;
@@ -23,17 +25,41 @@ public class TerrainGenerator : MonoBehaviour {
         int xRes = 513; int yRes = 513;
         float[,] heightmap = new float[xRes, yRes];
 
-        float scale_1 = 100.01f;
-        float scale_2 = 10.01f;
-        float scale_3 = 1.01f;
+        float x_offset = UnityEngine.Random.Range(0,100);
+        float y_offset = UnityEngine.Random.Range(0,100);
+
         for (int y = 0; y < yRes; y++) {
             for (int x = 0; x < xRes; x++) {
-                heightmap[x, y] = Mathf.PerlinNoise(x / scale_1, y / scale_1) * 0.25f;
-                heightmap[x, y] += Mathf.PerlinNoise(x / scale_2, y / scale_2) * 0.1f;
-                heightmap[x, y] += Mathf.PerlinNoise(x / scale_3, y / scale_3) * 0.01f;
+                heightmap[x, y] = Mathf.PerlinNoise(x / scale_1 + x_offset, y / scale_1 + y_offset) * height_1;
+                heightmap[x, y] += Mathf.PerlinNoise(x / scale_2 + y_offset, y / scale_2 + x_offset) * height_2;
+                heightmap[x, y] -= Mathf.PerlinNoise(x / scale_3 + x_offset, y / scale_3 + y_offset) * height_3;
+                
+                //heightmap[x, y] = (-Mathf.Sin(Mathf.Pow((float)Convert.ToDouble(x + 500)/300, 2)) + Mathf.Sin(Mathf.Pow((float)Convert.ToDouble(y + 500) / 300, 2)) + 5)*0.1f;
             }
         }
 
+        float averageHeight = 0;
+        for (int x = 0; x < xRes - 1; x++) {
+            for (int y = 0; y < yRes - 1; y++) {
+                averageHeight += heightmap[x, y];
+            }
+        }
+
+        averageHeight /= Mathf.Sqrt(heightmap.Length);
+        averageHeight *= 2;
+
+        Transform go = null;
+        try { go = Selection.activeGameObject.transform.Find("Water Level"); } catch { }
+
+        if (go == null) {
+            go = GameObject.CreatePrimitive(PrimitiveType.Plane).transform;
+            go.localScale = new Vector3(100, 1, 100);
+            go.name = "Water Level";
+            go.SetParent(Selection.activeGameObject.transform);
+        }
+
+        go.position = new Vector3(500, averageHeight * 0.3f, 500);
+        
         terrainData.SetHeights(0, 0, heightmap);
     }
 }
