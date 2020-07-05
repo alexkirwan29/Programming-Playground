34f4e80734c5897f74725be48dc34c521e5a5662
alexkirwan29 <alexkirwan29@gmail.com>
2020-07-05 12:39:19 +0800

    playerdirectory test

---
+++ PP-game/Assets/scripts/playerDirectory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class playerDirectory : MonoBehaviour
+{
+
+    static
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
