3c38563405ce2ad3881a47384231b5ad3ff28951
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-07-05 17:08:42 +0800

    Removed inheritance test script for player stat inheritance

--- PP-game/Assets/scripts/StatInheritanceTest.cs
+++
@@ -1,5 +0,0 @@
-﻿public class StatInheritanceTest : PlayerStats {
-    public override void InheritedAwake() { print("Awake"); }
-    public override void InheritedStart() { print("Start"); }
-    public override void InheritedUpdate(float time) { print("Update"); }
-}
