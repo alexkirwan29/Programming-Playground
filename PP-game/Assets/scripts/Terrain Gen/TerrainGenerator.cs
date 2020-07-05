570a0fba21aac456bb090942d765da3a3a9edf75
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-07-05 20:03:42 +0800

    Terrain generation core in progress

---
+++ PP-game/Assets/scripts/Terrain Gen/TerrainGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TerrainGenerator))]
+public class TerrainGeneratorInspector : Editor {
+    TerrainGenerator tg;
+
+    public override void OnInspectorGUI() {
+        if (GUILayout.Button("Generate New Terrain")) {
+            if (tg == null)
+                tg = new TerrainGenerator();
+            tg.GenerateNewTerrain();
+        }
+    }
+}
+
+public class TerrainGenerator : MonoBehaviour {
+    GameObject terrain;
+    
+    public void GenerateNewTerrain() {
+        print("Hello!");
+        if (terrain != null)
+            Destroy(terrain);
+
+        //We want to actually generate the terrain now
+        float[,] heightmap = new float[,];
+
+        TerrainData terraindata = new TerrainData();
+        terraindata.SetHeights(0,0,);
+        terrain = Terrain.CreateTerrainGameObject(terraindata);
+    }
+}
