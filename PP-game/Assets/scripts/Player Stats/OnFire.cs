4f4d1d25ad46cb6a2fc45d082edfc28a5c935215
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-07-06 22:27:26 +0800

    Moved all list update functions outside of update for all stat codes

--- PP-game/Assets/scripts/Player Stats/OnFire.cs
+++ PP-game/Assets/scripts/Player Stats/OnFire.cs
@@ -6,16 +6,18 @@ public class OnFire : PlayerStats_2 {
     public float damagePerSecond;
     
     public override void Inherited_Update(PlayerStats_2 returnScript) {
-        for (int i = 0; i < returnScript.stats.Count; i++) {
-            PlayerStats_2 ps_2 = returnScript.stats[i];
-            if ((ps_2.StatID == StatID) && (ps_2 != this))
-                returnScript.stats.Remove(ps_2);
-        }
-
         expiryTimer -= Time.deltaTime;
         if (expiryTimer <= 0)
             returnScript.stats.Remove(this);
 
         returnScript.health -= damagePerSecond * Time.deltaTime;
+    }
+
+    public override void ListWasUpdated(PlayerStats_2 returnScript) {
+        for (int i = 0; i < returnScript.stats.Count; i++) {
+            PlayerStats_2 ps_2 = returnScript.stats[i];
+            if ((ps_2.StatID == StatID) && (ps_2 != this))
+                returnScript.stats.Remove(ps_2);
+        }
     }
 }
--- PP-game/Assets/scripts/Player Stats/PlayerStats_2.cs
+++ PP-game/Assets/scripts/Player Stats/PlayerStats_2.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class PlayerStats_2 : ScriptableObject {
     public List<PlayerStats_2> stats = new List<PlayerStats_2>();
+    private List<PlayerStats_2> prevStats = new List<PlayerStats_2>();
+
     public float health;    //Didn't want this as a stat that can be added and removed, so it's just hardcoded
 
     public int StatID { get; set; }
+    public virtual void ListWasUpdated(PlayerStats_2 returnScript) { }
     public virtual void Inherited_Start(PlayerStats_2 returnScript) { }
     public virtual void Inherited_Awake(PlayerStats_2 returnScript) { }
     public virtual void Inherited_Update(PlayerStats_2 returnScript) { }
@@ -23,7 +26,17 @@ public class PlayerStats_2 : ScriptableObject {
     }
 
     public void Update() {
+        bool changeRecorded = false;
         for (int i = 0; i < stats.Count; i++) {
+            if (stats[i] != prevStats[i]) {
+                changeRecorded = true;
+                break;
+            }
+        }
+        
+        for (int i = 0; i < stats.Count; i++) {
+            if (changeRecorded)
+                stats[i].ListWasUpdated(this);
             stats[i].Inherited_Update(this);
         }
     }
