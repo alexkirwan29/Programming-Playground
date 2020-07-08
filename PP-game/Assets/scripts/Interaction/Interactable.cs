1a6b7e7e4ab21badae31d8a4e6df904b35474cd3
Those45Ninjas <tom.parker1235@gmail.com>
2020-07-08 19:41:57 +0800

    Started some interaction boiler-plate code

---
+++ PP-game/Assets/scripts/Interaction/Interactable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PP.Interact
+{
+  public class Interactable : MonoBehaviour
+  {
+    public UnityAction<Entity> OnInteract;
+
+    // The text that should appear when the user can interact with this.
+    public string InteractPrompt;
+
+    /// <summary>
+    /// Invoke the OnInteract event when this Interactable has been interacted with.
+    /// </summary>
+    /// <param name="entity">The Entity that is interacting with this intractable.</param>
+    public void Interact(Entity entity)
+    {
+
+      // Verify that the OnInteract 
+      if(OnInteract != null)
+        OnInteract.Invoke(entity);
+    }
+  }
+}
