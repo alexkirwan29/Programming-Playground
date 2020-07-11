a5f6ea380fbbc1f9084873d7f716395a6bcb8656
Those45Ninjas <tom.parker1235@gmail.com>
2020-07-11 15:43:48 +0800

    Cleaned up NetChat a little bit

--- PP-game/Assets/scripts/Networking/NetChat.cs
+++ PP-game/Assets/scripts/Networking/NetChat.cs
@@ -1,3 +1,6 @@
+// NetChat created by 45Ninjas aka Tom
+// Documentation at scripts/Networking/Doc/chat.md
+
 using System;
 
 namespace PP.Networking
@@ -10,11 +13,13 @@ namespace PP.Networking
       return controller != null && controller.IsRunning;
     }}
 
+    // The action that is called when a chat message has been received.
     public static Action<Packets.ChatMessage> OnMessageReceived;
 
     // The controller that this Class interacts with.
     private static GameNetworker controller;
 
+    // Initialises and subscribes to events.
     public static void Initialise(GameNetworker controller)
     {
       OnMessageReceived += debugChatMessage;
@@ -25,11 +30,13 @@ namespace PP.Networking
       NetChat.controller = controller;
     }
 
+    // Creates a debug message when a chat message has been received.
     private static void debugChatMessage(Packets.ChatMessage messagePacket)
     {
       UnityEngine.Debug.Log("Chat Message: " + messagePacket.message);
     }
 
+    // Un subscribes from events.
     public static void DeInitialise()
     {
       NetChat.controller.packetProcessor.RemoveSubscription<Packets.ChatMessage>();
--- PP-game/Assets/scripts/Networking/Packets/Chat.cs
+++ PP-game/Assets/scripts/Networking/Packets/Chat.cs
@@ -4,16 +4,8 @@ namespace PP.Networking.Packets
   public class ChatMessage
   {
     public string message {get; set;}
-
-    public ChatMessage()
-    {
-      
-    }
-
-    public ChatMessage(string message)
-    {
-      this.message = message;
-    }
+    
+    public ChatMessage() {}
+    public ChatMessage(string message) { this.message = message; }
   }
-
 }
