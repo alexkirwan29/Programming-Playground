7cc74fc00ed6928858fcae4fa6309aa24f36ce54
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-07-29 15:09:16 +0800

    Modified terrain generator to allow for textured ground

--- PP-game/Assets/scripts/TerrainGenerator.cs
+++ PP-game/Assets/scripts/TerrainGenerator.cs
@@ -7,25 +7,22 @@ public class TerrainGeneratorCustomInspector : Editor {
     int selected = 0;
 
     public override void OnInspectorGUI() {
-        string[] options = new string[] { "Standard Land", "Standard Island" };
-        selected = EditorGUILayout.Popup("Label", selected, options);
-
         DrawDefaultInspector();
 
         if (GUILayout.Button("Generate New Terrain")) {
             TerrainGenerator tg = new TerrainGenerator();
             tg.GenerateNewTerrain(
-                selected,
                 150, .15f,
                 50, 0.07f,
-                25, 0.03f
+                25, 0.03f,
+                2, 0.003f
             );
         }
     }
 }
 
 public class TerrainGenerator : MonoBehaviour {
-    public void GenerateNewTerrain(int landType, float scale_1, float height_1, float scale_2, float height_2, float scale_3, float height_3) {
+    public void GenerateNewTerrain(float scale_1, float height_1, float scale_2, float height_2, float scale_3, float height_3, float scale_4, float height_4) {
         //We want to actually generate the terrain now
         Terrain terrain = Selection.activeGameObject.GetComponent<Terrain>();
         TerrainData terrainData = terrain.terrainData;
@@ -35,9 +32,6 @@ public class TerrainGenerator : MonoBehaviour {
 
         float x_offset = UnityEngine.Random.Range(0,100);
         float y_offset = UnityEngine.Random.Range(0,100);
-
-        float lowestPoint = Mathf.Infinity;
-        float highestPoint = 0;
 
         for (int y = 0; y < yRes; y++) {
             for (int x = 0; x < xRes; x++) {
@@ -50,58 +44,11 @@ public class TerrainGenerator : MonoBehaviour {
                 //Height map 3
                 heightmap[x, y] -= Mathf.PerlinNoise(x / scale_3 + x_offset, y / scale_3 + y_offset) * height_3;
 
-                //Island calculations for height map correction
-                float x2 = (float)Convert.ToDouble(x) - 250;
-                float y2 = (float)Convert.ToDouble(y) - 250;
-                float distFromCenter = Mathf.Sqrt(Mathf.Pow(x2, 2) + Mathf.Pow(y2, 2));
-
-                switch (landType) {
-                    case 1:
-                        heightmap[x, y] *= cosNotRepeatingCorrected(distFromCenter / 90);
-                        break;
-                }
-
-                if (heightmap[x, y] < lowestPoint)
-                    lowestPoint = heightmap[x, y];
-                if (heightmap[x, y] > highestPoint)
-                    highestPoint = heightmap[x, y];
+                //Height map 4
+                heightmap[x, y] += Mathf.PerlinNoise(x / scale_4 + x_offset, y / scale_4 + y_offset) * height_4;
             }
         }
 
-        Transform go = null;
-        try { go = Selection.activeGameObject.transform.Find("Water Level"); } catch { }
-
-        if (go == null) {
-            go = GameObject.CreatePrimitive(PrimitiveType.Plane).transform;
-            go.localScale = new Vector3(100, 1, 100);
-            go.name = "Water Level";
-            go.SetParent(Selection.activeGameObject.transform);
-        }
-        
         terrainData.SetHeights(0, 0, heightmap);
-
-        print("Max: " + highestPoint + "      Min: " + lowestPoint);
-        float waterHeight = getPercentageOfHeight(highestPoint, lowestPoint, 0.25f);
-        float coastlineHeight = getPercentageOfHeight(highestPoint, lowestPoint, 0.2f);
-        float grassHeight = getPercentageOfHeight(highestPoint, lowestPoint, 0.3f);
-        float mountainsHeight = getPercentageOfHeight(highestPoint, lowestPoint, 0.5f);
-        float mountainTopHeight = getPercentageOfHeight(highestPoint, lowestPoint, 0.85f);
-
-        for (int y = 0; y < yRes; y++) {
-            for (int x = 0; x < xRes; x++) {
-                //We want to edit the texture based on height
-
-            }
-        }
-
-        go.position = new Vector3(500, waterHeight, 500);
-    }
-
-    public float cosNotRepeatingCorrected(float x) {
-        return Mathf.Cos(Mathf.Clamp(x,0,Mathf.PI))/2 + 0.5f;
-    }
-
-    public float getPercentageOfHeight(float max, float min, float percentage){
-        return (max - min) * percentage + min;
     }
 }
