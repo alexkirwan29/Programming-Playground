8399422a2fa1560fe1843f0d206afb62293c6837
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-08-02 19:50:50 +0800

    Moving into the branch

--- PP-game/Assets/scripts/JSONEditor.cs
+++ PP-game/Assets/scripts/JSONEditor.cs
@@ -1,15 +1,36 @@
 ﻿using System.Collections.Generic;
 using UnityEditor;
 
-struct JSONEntry {
-    int id;
-    string path;
-    string name;
+[System.Serializable]
+public class JSONEntry {
+    public int id;
+    public string path;
+    public string name;
+}
+
+public static class JSONManager {
+    static string JSONDir = "Assets/Objects/Entities/";
+
+    static void SaveData(List<JSONEntry> Entries) {
+        string result = "[";
+        foreach (JSONEntry i in Entries) {
+            result += "\n\t{";
+            result += "\n\t\t\"id\":"     + i.id + ",";
+            result += "\n\t\t\"path\":\"" + i.path + "\",";
+            result += "\n\t\t\"name\":\"" + i.name + "\"";
+            result += "\n\t},";
+        }
+
+        result += "\n]";
+
+        System.IO.File.WriteAllText(JSONDir,result);
+    }
 }
 
 public class JSONEditor : EditorWindow {
-    string JSONDir = "";
     List<JSONEntry> JSON_Entries = new List<JSONEntry>();
+    
+    int page = 0;
 
     [MenuItem("Window/Custom Windows/JSON Editor")]
     static void Init() {
@@ -19,7 +40,19 @@ public class JSONEditor : EditorWindow {
     }
 
     void OnGUI() {
+        if ((page == 0) && (JSON_Entries.Count != 0)) { page = 1; }
+        if ((page != 0) && (JSON_Entries.Count == 0)) { page = 0; }
+
         EditorGUILayout.LabelField("This window is a simple JSON interface");
-        JSONEntry s;
+        //JSONEntry e = JSON_Entries[0];
+
+
+
+
+
+
+
+
+        EditorGUILayout.LabelField("Item: " + page + "/" + JSON_Entries.Count);
     }
 }
