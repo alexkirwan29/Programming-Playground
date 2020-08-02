799136ff725ea3470c09fc1730527da354caf989
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-08-02 18:49:29 +0800

    Corrected Menu Directory

--- PP-game/Assets/scripts/JSONWindowController.cs
+++ PP-game/Assets/scripts/JSONWindowController.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEditor;
 
 public class JSONWindowController : EditorWindow {
-    
-    [MenuItem("Window/JSON Editor")]
+    [MenuItem("Window/Custom Windows/JSON Editor")]
     static void Init() {
         // Get existing open window or if none, make a new one:
         JSONWindowController window = (JSONWindowController)GetWindow(typeof(JSONWindowController));
         window.Show();
     }
 
+    void OnGUI() {
 
+    }
 }
