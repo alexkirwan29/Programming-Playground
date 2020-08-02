1bfe8a7070e50e4546b818af93c682844e9dd1c1
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-08-02 18:47:04 +0800

    Generated first window

---
+++ PP-game/Assets/scripts/JSONWindowController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+public class JSONWindowController : EditorWindow {
+    
+    [MenuItem("Window/JSON Editor")]
+    static void Init() {
+        // Get existing open window or if none, make a new one:
+        JSONWindowController window = (JSONWindowController)GetWindow(typeof(JSONWindowController));
+        window.Show();
+    }
+
+
+}
