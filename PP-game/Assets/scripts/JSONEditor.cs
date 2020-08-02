653a864471d6e3a4ba73c34191d02197442be803
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-08-02 19:14:08 +0800

    Moving into the branch

---
+++ PP-game/Assets/scripts/JSONEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+struct JSONEntry {
+    int id;
+    string path;
+    string name;
+}
+
+public class JSONEditor : EditorWindow {
+    string JSONDir = "";
+    List<JSONEntry> JSON_Entries = new List<JSONEntry>();
+
+    [MenuItem("Window/Custom Windows/JSON Editor")]
+    static void Init() {
+        // Get existing open window or if none, make a new one:
+        JSONEditor window = (JSONEditor)GetWindow(typeof(JSONEditor));
+        window.Show();
+    }
+
+    void OnGUI() {
+        EditorGUILayout.LabelField("This window is a simple JSON interface");
+        JSONEntry s;
+    }
+}
