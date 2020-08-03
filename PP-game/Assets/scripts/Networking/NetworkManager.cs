c8a9b0619c831c3f35892bb056036865d1e38705
Those45Ninjas <tom.parker1235@gmail.com>
2020-08-03 13:45:20 +0800

    Changed the way prefabs are stored and used.

--- PP-game/Assets/scripts/Networking/NetworkManager.cs
+++ PP-game/Assets/scripts/Networking/NetworkManager.cs
@@ -10,30 +10,19 @@ namespace PP.Networking
     public Server.GameServer Server;
     public Client.GameClient Client;
 
-    public EntityController entityController;
-
-    public NetworkedEntity testPrefab;
     public void Awake()
     {
-      var prefabs = new Dictionary<ushort, NetworkedEntity>();
-      prefabs.Add(100, testPrefab);
-
       if(Application.isBatchMode)
       {
         Server.gameObject.AddComponent<ServerConsole>();
-        Debug.Log("Starting as a Server");
         Destroy(Client);
-
+        Debug.Log("Starting as a Server");
         Server.Init();
-        entityController.Setup(Server, prefabs);
-        Server.Create();
       }
       else
       {
         Destroy(Server);
         Client.Init();        
-        Client.Create();
-        entityController.Setup(Client, prefabs);
         Client.Connect("localhost");
       }
     }
@@ -41,7 +30,7 @@ namespace PP.Networking
     private void Update()
     {
       if(Input.GetKeyDown(KeyCode.F1))
-        NetChat.SendMessage("Hello World! This is a test message sent from a client");
+        Client.Chat.BroadcastChatMessage("Hello World! This is a test message sent from a client");
     }
   }
 }
---
+++ PP-game/Assets/scripts/Networking/PrefabList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PP.Networking;
+
+namespace PP {
+
+  [CreateAssetMenu(menuName = "Programming Playground/Prefab List")]
+  public class PrefabList : ScriptableObject {
+
+    public List<Info> Prefabs;
+
+    [System.Serializable]
+    public class Info {
+      public NetworkedEntity Prefab;
+      public ushort Id;
+    }
+
+    /// <summary>
+    /// Get all the prefabs in this list as a Dictionary.
+    /// </summary>
+    /// <returns>All the prefabs.</returns>
+    public Dictionary<ushort, NetworkedEntity> GetPrefabs() {
+      var dict = new Dictionary<ushort, NetworkedEntity>();
+
+      foreach (var item in Prefabs) {
+        dict.Add(item.Id, item.Prefab);
+      }
+
+      return dict;
+    }
+  }
+}
